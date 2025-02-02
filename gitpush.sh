6c96d3e316797c1f5b5e54563e705300c7d9db94
oliLamRou <parties01smacks@icloud.com>
2025-02-02 16:15:01 -0500

    setup wip

--- gitpush.sh
+++ gitpush.sh
@@ -2,9 +2,9 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate web
 echo "Active Conda Env: $(conda info --envs | grep '*' | awk '{print $1}')"
 
-if git rev-parse --verify data > /dev/null 2> ./log.txt; then
+if git rev-parse --verify csv > /dev/null 2> ./log.txt; then
 echo "Starting Commit"
-git checkout data
+git checkout csv
 git add ./data/data.csv
 git commit -m 'update data'
 git push
