7c23d17e6565ae44f5f40c5afa9f1e2aa9e2b64b
oliLamRou <parties01smacks@icloud.com>
2025-02-02 15:42:40 -0500

    basic process py

--- backend/python/data_process.py
+++ backend/python/data_process.py
@@ -1,7 +1,15 @@
+import os
 import pandas as pd
+import datetime
 
 from backend import DATADIR
 filename = DATADIR + '/data.csv'
+date = str(datetime.datetime.now())
 
-df = pd.DataFrame(columns=['name', 'age'])
+df = pd.DataFrame(columns=['date'])
+if os.path.exists(filename):
+    df = pd.read_csv(filename)
+
+df.loc[len(df.index)] = [date]
 df.to_csv(filename, index=False)
+
